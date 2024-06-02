5e31f9ce0d54f26cb076104fd9dcd43965c64d58
Agus <agusalfandi8@gmail.com>
2024-06-02 20:25:24 +0700

    refactor:penyesuaian style

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import { Link } from "react-router-dom";
 function App() {
   return (
     <div className="flex justify-center items-center h-screen gap-4">
-      <button className="border rounded-lg p-4 bg-slate-500">
-        <Link to="/Portofolio">Portofolio web</Link>
-      </button>
-      <button className="border rounded-lg p-4 bg-slate-500">
-        <Link to="/Todo">UI Todo list</Link>
-      </button>
+      <Link to="/Portofolio">
+        <button className="btn btn-neutral">Portofolio web</button>
+      </Link>
+      <Link to="/Todo">
+        <button className="btn btn-neutral">UI Todo list</button>
+      </Link>
     </div>
   );
 }
